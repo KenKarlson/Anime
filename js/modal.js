7feea07b544af94a275133b1ff199a71be0d46c0
Ken Karlson <kenkarlson@mail.ru>
2024-02-28 01:45:07 +0300

    Change modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,11 @@
 //Open modal block
 "use strict";
 const modal = document.querySelector(".search-model");
-const btn = document.querySelector(".icon_search");
+const searchBtn = document.querySelector(".icon_search");
 const closeBtn = modal.querySelector(".search-close-switch");
 
-btn.addEventListener("click", () => {
-  modal.style.display === "none"
-    ? (modal.style.display = "block")
-    : (modal.style.display = "none");
-  //console.log("click");
+searchBtn.addEventListener("click", () => {
+  modal.style.display = "block";
 });
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
