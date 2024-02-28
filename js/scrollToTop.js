409dd6a59f7072a93351748f8e266e8d3a541fa3
Ken Karlson <kenkarlson@mail.ru>
2024-02-28 14:30:26 +0300

    Step 2 - add scroll to top

---
+++ js/scrollToTop.js
@@ -0,0 +1,10 @@
+const scrollToTopButton = document.querySelector("#scrollToTopButton");
+
+scrollToTopButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  seamless.scrollIntoView(document.querySelector(".header"), {
+    behavior: "smooth",
+    block: "center",
+    inline: "center",
+  });
+});
