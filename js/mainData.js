ba8c08be6dd73b39f9fe505addb812a70a0a99ad
Ken Karlson <kenkarlson@mail.ru>
2024-02-29 16:43:05 +0300

    Add fr.db.

--- js/mainData.js
+++ js/mainData.js
@@ -1,6 +1,6 @@
 const mainData = () => {
-  fetch("../db.json")
+  fetch("https://anime-s0a5-default-rtdb.firebaseio.com/anime.json")
     .then((res) => res.json())
-    .then((data) => console.log(data.anime));
+    .then((data) => console.log(data));
 };
 mainData();
