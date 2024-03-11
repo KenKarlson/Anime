fe0e4abf99b32a88fd6811183e0b92c4f5c0f4c9
Ken Karlson <kenkarlson@mail.ru>
2024-03-11 13:56:31 +0300

    Add ganres

--- js/mainData.js
+++ js/mainData.js
@@ -1,19 +1,26 @@
 const mainData = () => {
+  const renderAnimeList = (array, ganres )=>{
+    console.log(array, ganres);
+  };
   const renderTopAnime = (array)=>{
     const wrapper = document.querySelector('.filter__gallery');
     wrapper.innerHTML = '';
     array.forEach(element => {
-      wrapper.insertAdjacentHTML('beforebegin',`
-      <div class="product__sidebar__view__item set-bg mix" >
-        <div class="ep">18 / &?</div>
+      wrapper.insertAdjacentHTML('afterbegin',`
+      <div class="product__sidebar__view__item set-bg mix" data-setbg="${element.image}">
+        <div class="ep">${element.rating} / 10</div>
         <div class="view"><i class="fa fa-eye"></i>${element.views}</div>
-        <h5><a href="">Name</a></h5>  
+        <h5><a href="">${element.title}</a></h5>  
       </div>
       `);
-        console.log(element.image+'\n'+element.views);
+        //console.log(element.image+'\n'+element.views);
+        wrapper.querySelectorAll('.set-bg').forEach((elem)=>{
+          elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+        });
     });
-    //console.log(array);
-  };;
+    //const imgWr = document.querySelector('.');
+    console.log(wrapper);
+  };
 
 
   fetch('../new-db.json')
@@ -21,9 +28,16 @@ const mainData = () => {
       return response.json();
     })
     .then((data) => {
-      renderTopAnime(data.anime.sort((a, b)=>{ 
+      const ganres = new Set();
+      renderTopAnime(data.anime.sort((b, a)=>{ 
         return b.views - a.views
       }).slice(0,7));
+
+      data.anime.forEach((item)=>{
+        ganres.add(item.ganre);
+      });
+      renderAnimeList(data.anime, ganres);
+      //console.log(ganres);
     });
 };
 mainData();
